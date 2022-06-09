927d0e8389042276372b4e7ea10869c7fb55b947
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-06-09 18:35:03 -0300

    test: adjusts event test

--- tests/factories/events-factory.ts
+++ tests/factories/events-factory.ts
@@ -10,22 +10,16 @@ export type EventParams = {
   endsAt?: string;
 };
 
-export function createEvent(params?: any) {
-  // return redis.set(
-  //   'event',
-  //   JSON.stringify({
-  //     title: params.title || JSON.stringify(faker.lorem.sentence()),
-  //     backgroundImageUrl: params.backgroundImageUrl || JSON.stringify(faker.image.imageUrl()),
-  //     logoImageUrl: params.logoImageUrl || JSON.stringify(faker.image.imageUrl()),
-  //     startsAt: params.startsAt || JSON.stringify(dayjs().subtract(1, 'day').toDate()),
-  //     endsAt: params.endsAt || JSON.stringify(dayjs().add(5, 'days').toDate()),
-  //   }),
-  // );
-  return redis.hSet('event', {
-    title: params.title || JSON.stringify(faker.lorem.sentence()),
-    backgroundImageUrl: params.backgroundImageUrl || JSON.stringify(faker.image.imageUrl()),
-    logoImageUrl: params.logoImageUrl || JSON.stringify(faker.image.imageUrl()),
-    startsAt: params.startsAt || JSON.stringify(dayjs().subtract(1, 'day').toDate()),
-    endsAt: params.endsAt || JSON.stringify(dayjs().add(5, 'days').toDate()),
+export async function createEvent(params?: EventParams) {
+  await redis.hSet('event', {
+    title: params?.title || JSON.stringify(faker.lorem.sentence()),
+    backgroundImageUrl: params?.backgroundImageUrl || JSON.stringify(faker.image.imageUrl()),
+    logoImageUrl: params?.logoImageUrl || JSON.stringify(faker.image.imageUrl()),
+    startsAt: params?.startsAt || JSON.stringify(dayjs().subtract(1, 'day').toDate()),
+    endsAt: params?.endsAt || JSON.stringify(dayjs().add(5, 'days').toDate()),
   });
+
+  const event = await redis.hGetAll('event');
+
+  return event;
 }
